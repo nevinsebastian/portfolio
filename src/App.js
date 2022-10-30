d8101c3b3bdf8dc84fdea45b7a9e858566d82c0c
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2022-10-30 19:49:40 +0530

    added nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import NavBar from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      
+      <NavBar/>
     </div>
   );
 }
